41dd28c1e3569254c55f9f15adc4f9ca4cca444c
vishaljangid786 <vishaljangid80550786@gmail.com>
2025-01-31 10:46:29 +0530

    done

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,12 @@
 import { NavLink } from "react-router-dom";
 
-import { logo } from "../assets/images";
+// import { logo } from "../assets/images";
 
 const Navbar = () => {
   return (
     <header className='header'>
       <NavLink to='/'>
-        <img src={logo} alt='logo' className='w-18 h-18 object-contain' />
+       <h1 className="text-xl font-semibold shadow-lg p-3 rounded-lg">VJ</h1>
       </NavLink>
       <nav className='flex text-lg gap-7 font-medium'>
         <NavLink to='/about' className={({ isActive }) => isActive ? "text-blue-600" : "text-black" }>
